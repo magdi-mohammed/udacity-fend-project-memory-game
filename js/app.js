0967fe94d602446c162f23d425d4e500ab44d50d
Magdi-Mohammed <magdy_291@yahoo.com>
2018-04-08 17:02:39 +0200

    i added those three functions .. display symbol .. open cards .. matched cards

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ function shuffle(array) {
 }
 
 // for debug
-cards.addClass(' open show');
+// cards.addClass(' open show');
 
 // shuffle cards using shuffle function
 cards = shuffle(cards);
@@ -43,3 +43,54 @@ for (let card of cards){
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+cards.each(function () {
+  $(this).on('click', function () {
+    displaySymbol($(this));
+    openCards($(this));
+    matchedCards($(this));
+  });
+});
+// display the card symbol function
+function displaySymbol(element) {
+  element.addClass('open show');
+}
+
+// list of open cards
+// firstElement is the random first open card , i used it to compare with the second element showen by click
+let firstElement = $('.card.open.show');
+let openCardsArr = [firstElement];
+
+function openCards(element) {
+  openCardsArr.push(element);
+}
+
+/*
+// to do :  بلفيه مشكلة في الفانكشن دي .. مفروض لما ادوس علي كارت يقارنه بالكارت ال دوست عليه قبله و لو  مطلعش زيه يعيد الفانكشن من الاول بحييث يقارن بين ال دوست عليه بعد م الفانكشن اشتغلت من الوال مش آخر واحد دوست عليه وخلا
+*/
+
+// if the open cards array has another open card .. check of the two cards is matched
+function matchedCards(element) {
+  // if condition checks if the openCardsArr is empty or not
+  if (typeof openCardsArr !== 'undefined' && openCardsArr.length > 0) {
+    let clickedElement = element;
+    let openCardElement =  openCardsArr[openCardsArr.length - 2];
+    let clickedClass = element.children().attr('class');
+    let openCardClass = openCardsArr[openCardsArr.length - 2].children().attr('class');
+
+      if (clickedClass === openCardClass) {
+        window.console.log('matched');
+
+        clickedElement.removeClass('open show');
+        clickedElement.addClass('match');
+
+        openCardElement.removeClass('open show');
+        openCardElement.addClass('match');
+
+      } else if (clickedClass !== openCardClass) {
+        window.console.log('Dosn\'t match');
+        clickedElement.removeClass('open show');
+        openCardElement.removeClass('open show');
+      }
+  }
+}
